2db12e0785b2899c1de860dc57ee7dcd8022ddf3
Nataly <cerise@inbox.ru>
2022-02-18 22:40:19 +0300

    Переносит компоненты страниц в директорию pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,10 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import {AppRoute, AuthorizationStatus} from '../../const';
-import MainScreen from '../main-screen/main-screen';
-import LoginScreen from '../login-screen/login-screen';
-import FavoritesScreen from '../favorites-screen/favorites-screen';
-import RoomScreen from '../room-screen/room-screen';
-import NotFoundScreen from '../not-found-screen/not-found-screen';
+import MainScreen from '../../pages/main-screen/main-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import RoomScreen from '../../pages/room-screen/room-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import PrivateRoute from '../private-route/private-route';
 
 type AppPlacesCountProps = {
