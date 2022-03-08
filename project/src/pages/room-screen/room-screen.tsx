dc71ad98e89b426fa294bdbfe5e83acfc6c6bd76
Nataly <cerise@inbox.ru>
2022-03-08 10:02:48 +0300

    Создает компоненты Список отзывов и Отзыв

--- project/src/pages/room-screen/room-screen.tsx
+++ project/src/pages/room-screen/room-screen.tsx
@@ -4,6 +4,7 @@ import {Offer} from '../../types/offer';
 import {AppRoute} from '../../const';
 import {getPlaceRatingStars, getPlaceType} from '../../utils/card';
 import {useParams, Navigate} from 'react-router-dom';
+import ReviewsList from '../../components/reviews-list/reviews-list';
 
 type RoomScreenProps = {
   offers: Offer[];
@@ -132,35 +133,7 @@ function RoomScreen({offers}: RoomScreenProps): JSX.Element {
               </div>
               <section className="property__reviews reviews">
                 <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-                <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
-                      </div>
-                      <span className="reviews__user-name">
-                        Max
-                      </span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span
-                            style={{
-                              width: '80%',
-                            }}
-                          >
-                          </span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                    </div>
-                  </li>
-                </ul>
+                <ReviewsList />
                 <ReviewForm />
               </section>
             </div>
